06e78f812c520474e9f58c8a7aed807e328baa69
Thiago Paes <mrprompt@gmail.com>
2011-01-29 18:12:16 -0200

    desrição e título adicionados

--- jquery.youtube-0.1/jquery.youtube-0.1.js
+++ jquery.youtube-0.1/jquery.youtube-0.1.js
@@ -15,14 +15,19 @@ var Youtube = {
             $(Youtube.container).html('');
 
             var yt = data.feed.entry;
+            var detalhes = new Array();
 
             for (var i = 0; i < yt.length; i++) {
+                detalhes[i]    = new Array();
+                detalhes[i][0] = yt[i].title.$t;
+                detalhes[i][1] = yt[i].content.$t;
+                detalhes[i][2] = yt[i].media$group.media$content[0].url;
+
                 $('<a/>').attr('href', yt[i].link[0].href)
-                      .attr('title', yt[i].title.$t)
-                      .attr('id', 'video_' + i)
-                      .attr('rel', yt[i].media$group.media$content[0].url)
-                      .addClass('video')
-                      .appendTo(Youtube.container);
+                         .attr('title', yt[i].title.$t)
+                         .attr('id', 'video_' + i)
+                         .addClass('video')
+                         .appendTo(Youtube.container);
 
                 $("<img/>").attr("src", yt[i].media$group.media$thumbnail[1].url)
                            .attr('alt', yt[i].title.$t)
@@ -32,8 +37,14 @@ var Youtube = {
                          .appendTo('#video_' + i);
             }
 
+            // capturando o click dos thumbs
             $(Youtube.container + ' a').click(function(e) {
                 e.preventDefault();
+
+                var indice    = $(this).attr('id').replace('video_', '');
+                var descricao = detalhes[indice][1];
+                var titulo    = detalhes[indice][0];
+                var endereco  = detalhes[indice][2];
 
                 if ($('#player')[0]) {
                     $('#player').remove();
@@ -46,7 +57,7 @@ var Youtube = {
                               .appendTo(Youtube.container);
 
                 $('<param/>').attr('name', 'movie')
-                             .attr('value', $(this).attr('rel'))
+                             .attr('value', endereco)
                              .appendTo('#player');
 
                 $('<param/>').attr('allowFullScreen', 'true')
@@ -55,13 +66,16 @@ var Youtube = {
                 $('<param/>').attr('allowscriptaccess', 'always')
                              .appendTo('#player');
 
-                $('<embed/>').attr('src', $(this).attr('rel'))
+                $('<embed/>').attr('src', endereco)
                               .attr('type', 'application/x-shockwave-flash')
                               .attr('allowscriptaccess', 'always')
                               .attr('allowfullscreen', 'true')
                               .attr('width', Youtube.playerLargura)
                               .attr('height', Youtube.playerAltura)
                               .appendTo('#player');
+
+                $('<p/>').html('<b>' + titulo + '</b><br />' + descricao)
+                         .appendTo('#player');
             })
         });
     }
